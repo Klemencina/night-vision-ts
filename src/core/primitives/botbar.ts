3038310a8f9992d4b00b6ac471d169ed93beeb45
Klemencina <56873773+Klemencina@users.noreply.github.com>
2026-02-03 22:17:11 +0100

    overlaping bootbar fix

--- src/core/primitives/botbar.ts
+++ src/core/primitives/botbar.ts
@@ -1,14 +1,9 @@
-
 // Drawing botbar with CanvasJS
 
 import Const from '../../stuff/constants'
 import Utils from '../../stuff/utils'
 
-const {
-    MINUTE15, HOUR,
-    DAY, WEEK, MONTH, YEAR,
-    MONTHMAP, HPX
-} = Const
+const { MINUTE15, HOUR, DAY, WEEK, MONTH, YEAR, MONTHMAP, HPX } = Const
 
 interface Props {
     config: {
@@ -41,9 +36,8 @@ interface Layout {
 }
 
 function body(props: Props, layout: Layout, ctx: CanvasRenderingContext2D) {
-
     if (!ctx) return // Guard against null context
-    
+
     const width = layout.botbar.width
     const height = layout.botbar.height
 
@@ -63,30 +57,85 @@ function body(props: Props, layout: Layout, ctx: CanvasRenderingContext2D) {
     ctx.fillStyle = props.colors.text
     ctx.beginPath()
 
-    for (var p of layout.botbar.xs) {
+    type BotbarLabel = {
+        index: number
+        x: number
+        lbl: string
+        width: number
+        priority: number
+        left: number
+        right: number
+    }
 
-        let lbl = formatDate(props, p)
-        let x = p[0] + sb0
+    const labels: BotbarLabel[] = []
+    const labelPadding = 6
+
+    function labelPriority(tf: number) {
+        if (tf === YEAR) return 4
+        if (tf === MONTH) return 3
+        if (tf === DAY) return 2
+        return 1
+    }
+
+    for (let i = 0; i < layout.botbar.xs.length; i++) {
+        const p = layout.botbar.xs[i]
+        const lbl = String(formatDate(props, p))
+        const x = p[0] + sb0
+        const width = ctx.measureText(lbl).width
+        const left = x - width * 0.5 - labelPadding
+        const right = x + width * 0.5 + labelPadding
+        labels.push({
+            index: i,
+            x,
+            lbl,
+            width,
+            priority: labelPriority(p[2]),
+            left,
+            right
+        })
+    }
+
+    const accepted: BotbarLabel[] = []
+    for (const label of labels) {
+        let keep = true
+        while (accepted.length) {
+            const last = accepted[accepted.length - 1]
+            if (label.left > last.right) break
+            if (label.priority > last.priority) {
+                accepted.pop()
+                continue
+            }
+            keep = false
+            break
+        }
+        if (keep) accepted.push(label)
+    }
+
+    const acceptedIdx = new Set(accepted.map(label => label.index))
+
+    for (let i = 0; i < layout.botbar.xs.length; i++) {
+        const p = layout.botbar.xs[i]
+        const x = p[0] + sb0
         //if (p[0] - sb0 > width - sb1) continue
 
         ctx.moveTo(x + HPX, 0)
         ctx.lineTo(x + HPX, 4.5)
 
-        if (!lblHighlight(props, p[1][0])) {
-            ctx.globalAlpha = 0.85
+        if (acceptedIdx.has(i)) {
+            const lbl = labels[i].lbl
+            if (!lblHighlight(props, p[1][0])) {
+                ctx.globalAlpha = 0.85
+            }
+            ctx.textAlign = 'center'
+            ctx.fillText(String(lbl), x, 18)
+            ctx.globalAlpha = 1
         }
-        ctx.textAlign = 'center'
-        ctx.fillText(String(lbl), x, 18)
-        ctx.globalAlpha = 1
-
     }
 
     ctx.stroke()
-
 }
 
 function panel(props: Props, layout: Layout, ctx: CanvasRenderingContext2D) {
-
     if (!ctx) return // Guard against null context
     let lbl = formatCursorX(props)
     ctx.fillStyle = props.colors.panel
@@ -106,11 +155,9 @@ function panel(props: Props, layout: Layout, ctx: CanvasRenderingContext2D) {
     ctx.fillStyle = props.colors.textHL
     ctx.textAlign = 'center'
     ctx.fillText(String(lbl), cursor, y + 16)
-
 }
 
 function formatDate(props: Props, p: number[]) {
-
     let t = p[1]
     let tf = props.timeFrame
 
@@ -132,12 +179,10 @@ function formatDate(props: Props, p: number[]) {
 
     let h = Utils.addZero(d.getUTCHours())
     let m = Utils.addZero(d.getUTCMinutes())
-    return h + ":" + m
-
+    return h + ':' + m
 }
 
 function formatCursorX(props: Props) {
-
     let t = props.cursor.time
     if (t === undefined) return `Out of range`
     // TODO: IMPLEMENT TI
@@ -168,11 +213,10 @@ function formatCursorX(props: Props) {
     if (tf < DAY) {
         let h = Utils.addZero(d.getUTCHours())
         let m = Utils.addZero(d.getUTCMinutes())
-        time = h + ":" + m
+        time = h + ':' + m
     }
 
     return `${date}  ${time}`
-
 }
 
 // Highlights the begining of a time interval
@@ -181,7 +225,6 @@ function formatCursorX(props: Props) {
 // will be no month name on t-axis. Sad.
 // Solution: manipulate the grid, skew it, you know
 function lblHighlight(props: Props, t: number) {
-
     let tf = props.timeFrame
 
     if (t === 0) return true
@@ -190,18 +233,24 @@ function lblHighlight(props: Props, t: number) {
     if (tf <= MINUTE15 && t % HOUR === 0) return true
 
     return false
-
 }
 
-function roundRect(ctx: CanvasRenderingContext2D, x: number, y: number, w: number, h: number, r: number) {
+function roundRect(
+    ctx: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    w: number,
+    h: number,
+    r: number
+) {
     if (w < 2 * r) r = w / 2
     if (h < 2 * r) r = h / 2
     ctx.beginPath()
-    ctx.moveTo(x+r, y)
-    ctx.arcTo(x+w, y,   x+w, y+h, 0)
-    ctx.arcTo(x+w, y+h, x,   y+h, r)
-    ctx.arcTo(x,   y+h, x,   y,   r)
-    ctx.arcTo(x,   y,   x+w, y,   0)
+    ctx.moveTo(x + r, y)
+    ctx.arcTo(x + w, y, x + w, y + h, 0)
+    ctx.arcTo(x + w, y + h, x, y + h, r)
+    ctx.arcTo(x, y + h, x, y, r)
+    ctx.arcTo(x, y, x + w, y, 0)
     ctx.closePath()
     ctx.fill()
 }
